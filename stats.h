12ef574c6895af17f56441d220d3fe1e2c952634
MatheswaranPalanisamy <115540968+MatheswaranPalanisamy@users.noreply.github.com>
2022-10-12 10:18:18 +0530

    Update stats.h
    Update definitions of emailAlerter and ledAlerter functions

--- stats.h
+++ stats.h
@@ -10,5 +10,8 @@ struct Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
+void emailAlerter(void);
+void ledAlerter(void);
+
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
