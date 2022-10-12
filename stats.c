878553b0c342ca01a0441dc15f9957e88dce6ac1
MatheswaranPalanisamy <115540968+MatheswaranPalanisamy@users.noreply.github.com>
2022-10-12 10:35:16 +0530

    Update stats.c
    Update check and alert function for alerter logic

--- stats.c
+++ stats.c
@@ -5,6 +5,8 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
@@ -15,8 +17,25 @@ void emailAlerter(void)
     emailAlertCallCount++;
 }
 
-
 void ledAlerter(void)
 {
     ledAlertCallCount++;
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    int setlength = sizeof(alerters) / sizeof(alerters[0])
+    int counter = 0;
+
+    if(computedStats.max > maxThreshold)
+    {
+        while(counter < setlength)
+        {
+            if(alerters[counter] != (void *)0)
+            {
+                alerters[counter]();
+            }
+            counter++;
+        }
+    }
 }
